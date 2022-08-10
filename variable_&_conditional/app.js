238a6e5d929143bc09b4eb75d09b9519798a2ef7
Eon Slemp <eonslemp@gmail.com>
2022-08-10 15:04:12 -0400

    variables and conditionals practice

---
+++ variable_&_conditional/app.js
@@ -0,0 +1,93 @@
+
+// good
+let age;
+
+// bad
+var gae;
+
+const ega = 7;
+
+const name = 'Eon'
+
+const male = true;
+const female = false;
+
+const example = undefined;
+const example_2 = null;
+
+//undefined and null are interchangable
+
+/* assignment operators
+string operators
+logical operators
+bitwise operators
+
+*/
+
+let first = 'bob'
+let last = 'jenkins'
+let full = first + last
+let concat = 'bob'+'+'+'jenkins'
+// AND
+
+//console.log(concat)
+let x = 44; 
+let y = 78;
+//console.log(x + y < 20 || x+y > 20)
+
+// camel case = exPascalCase
+// ex_snake_case
+// kebab case = kebab-case-ex
+
+/*logical operators
+AND &&
+OR ||
+NOT !
+*/
+
+let poop = 0;
+poop = poop - 8
+poop += 23
+//console.log(poop++)
+//loose comparison is double ==, (only checks value)
+// strict comparison is triple ===, (checks type and value )
+//assignment is single = 
+/*
+ */
+let ex = '44'
+//console.log(x == ex)
+
+let p = 34;
+let q = 59;
+let k = p + q;
+let i = q++;
+let j = 1776;
+console.log(q)
+let a = 1;
+let b = 2; 
+let c = 3;
+let d = 4;
+let e = 'giraffe';
+let f = 5456;
+let g = (p+q+k+i+j)/5;
+let u = (a+b+c+d+f)/5
+//console.log(g)
+//console.log(u)
+//console.log((g+u)/2)
+
+s = (4+10+22-30+55)/5;
+t = (68 +7-22+9+100)/5
+
+//console.log(s)
+//console.log(t)
+//console.log((s+t)/2)
+
+box1 = 'box1';
+box2 = 'box2';
+temp = undefined;
+
+temp = box2;
+box2 = box1;
+box1 = temp;
+//console.log(box1, box2)
+
---
+++ variable_&_conditional/exercize.js
@@ -0,0 +1,63 @@
+/*box1 = "box 2!";
+box2 = "box 1!";
+temp = undefined;
+
+temp = box1;
+box1 = box2;
+box2 = temp;
+console.log(box1, box2)
+
+// conditionals
+/*
+let x = '10';
+if (x < 10){
+console.log('True')
+} else if(x > 10) {console.log('Falsified Information')
+} else {
+console.log('equal') 
+}
+*/
+
+// let y = 4;
+// if (y > 0){
+// console.log('+ poitive + ')
+// } else if (y < 0) {
+// console.log('- negative - ')
+// } 
+
+
+// let age = 8;
+// // let age = 18;
+
+// if (age >= 18){
+// console.log('access granted')
+// } 
+// else if (age < 18) {
+//     console.log('access denied')
+// }
+let num = -100;
+if (num > 0) {
+    if (num < 100) {
+        console.log('variable is greater than 0 and less than 100')
+    } else {
+        console.log('number is greater than zero and greater than or equal to 100')
+    }
+} else {
+    console.log('number is negative')
+}
+
+grade = 65;
+
+if (grade >= 90) {
+console.log('A')
+} else if (grade >= 80){
+    console.log('B')
+} else if (grade >= 70){
+    console.log('C')
+} else if (grade >= 55 ){
+    console.log("D")
+} else{
+    console.log('F')
+}
+
+
