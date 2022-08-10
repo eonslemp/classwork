3cca709b415fff9dc444978fc509faddf951ec58
Eon Slemp <eonslemp@gmail.com>
2022-08-10 18:20:49 -0400

    aug 10 classwork

--- variable_&_conditional/exercize.js
+++ variable_&_conditional/exercize.js
@@ -35,29 +35,100 @@ console.log('equal')
 // else if (age < 18) {
 //     console.log('access denied')
 // }
-let num = -100;
-if (num > 0) {
-    if (num < 100) {
-        console.log('variable is greater than 0 and less than 100')
-    } else {
-        console.log('number is greater than zero and greater than or equal to 100')
-    }
-} else {
-    console.log('number is negative')
-}
+// let num = -100;
+// if (num > 0) {
+//     if (num < 100) {
+//         console.log('variable is greater than 0 and less than 100')
+//     } else {
+//         console.log('number is greater than zero and greater than or equal to 100')
+//     }
+// } else {
+//     console.log('number is negative')
+// }
 
-grade = 65;
+// grade = 65;
 
-if (grade >= 90) {
-console.log('A')
-} else if (grade >= 80){
-    console.log('B')
-} else if (grade >= 70){
-    console.log('C')
-} else if (grade >= 55 ){
-    console.log("D")
-} else{
-    console.log('F')
-}
+// if (grade >= 90) {
+// console.log('A')
+// } else if (grade >= 80){
+//     console.log('B')
+// } else if (grade >= 70){
+//     console.log('C')
+// } else if (grade >= 55 ){
+//     console.log("D")
+// } else{
+//     console.log('F')
+// 
 
+// let global = 'global';
 
+// if (true) {
+//     let block_scoped = 'block';
+//     if (true) {
+//         console.log(block_scoped)
+//         if (true) {
+
+//         }
+//     }
+// }
+// let number = 5;
+
+// switch (number){
+//     case 1:  
+//         console.log('case 1')
+//         break;
+//     case 2:  
+//         console.log('case 2')
+//         break;
+//     case 3:
+//         console.log('case 3')
+//         break;
+//     default:
+//         console.log('default case')
+//         break;
+// }
+
+// number_2 = 2;
+
+// switch (number_2){
+//     case 1:  
+//     case 2:  
+//     case 3:
+//         console.log('case 1, 2, or 3')
+//         break;
+//     default:
+//         console.log('default case')
+//         break;
+// }
+
+// ternary operator
+
+let lighting = 'dark'
+
+time_of_day = 
+    lighting === "dark" ?  
+    "nighttime ": "daytime";
+
+// console.log(time_of_day)
+
+// letter = 'X'
+
+// switch(letter) {
+//     case 'A':
+//     case 'E':
+//     case 'I':
+//     case 'O':
+//     case 'U':
+//     case 'Y':
+//         console.log('vowel')
+//         break;
+//     default:
+//         console.log('not vowel');
+//         break;
+    
+
+// //     };
+let number = -1;
+
+statuss = number == 0 ? 'equals 0':(number > 0 ? '+ positive +':'- negative -');
+console.log(statuss)
