5cea483596ec0acc7e2ffa3bd55da31acc64073c
Eon Slemp <eonslemp@gmail.com>
2022-08-11 15:59:03 -0400

    loops classwork

--- loops/app.js
+++ loops/app.js
@@ -1,3 +1,90 @@
-for (let i = 0; i < 10; i+=2){
-    console.log(i)
+// // for (let i = 0; i < 10; i+=2){
+// //     console.log(i)
+// // }
+
+// for (let i = 13; i > 0; i-=1){
+//     console.log(i)
+//     if (i == 13){
+//         console.log('lucky number 13!')
+//     }
+// }
+
+// ctrl + c is keyboard interupt
+
+// for (let i = 0;; i++){
+//     console.log(i)
+//     if (i == 5){
+//         break;
+//     }else{
+//         continue;
+//     }
+// }  
+//exercize 1
+// for (let i = 10; i > 1; i-=1)
+// {
+//     console.log(i)
+// }
+//exercize 2
+
+// for (let i = 1; i < 10; i +=2)
+// {
+//     console.log(i)
+// }
+// for (let i = 2; i <= 10; i +=2){
+//     console.log(i)
+// }
+//exerrcize 3
+// for (let i = 6; i <= 60; i += 3){
+//     console.log(i)
+// }
+
+// i = 3
+// k = 'k'
+// console.log(k*1)
+// exercize 4
+// for (let i = 1; i <=7; i++){
+//     string = "#";
+//     console.log(string.repeat( i ));
+// }
+//exercize 5
+// for (let i = 2; i <= 10; i +=2){
+//     if(i % 2 == 0){
+//     console.log(i)
+//     }
+// }
+
+// for (let i = 2; i <=20; i ++){
+//     console.log(i)
+// }
+
+//while loops
+// let i = 1;
+// while(i < 5){
+// console.log(i)
+// i++
+// }
+
+
+// i = 3;
+// while(i <= 35){
+//         console.log(i)
+//         i+=3
+//     }
+
+
+// k = 5;
+// while (k < 100){
+
+//     console.log(k)
+//     k +=5
+// }
+
+j = 1;
+while(j <= 20){
+    if(j%2 ==0){
+    console.log(3*j)
+    }
+    j++
+
 }
+
